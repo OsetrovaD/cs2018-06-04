418f41d7362d137cf900320aac72a9d4ea9e3282
Osetrova <liberty_20_07@mail.ru>
2018-06-14 23:16:26 +0300

    lesson 5

--- src/by/it/osetrova/lesson05/TaskC2.java
+++ src/by/it/osetrova/lesson05/TaskC2.java
@@ -4,15 +4,40 @@ package by.it.osetrova.lesson05;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 
-public class TaskC2 {
-    //Напишите тут ваш код
+import java.lang.reflect.Array;
+import java.util.*;
 
-
-
-
-
-    public static void sort(int[] array) {
-        //Напишите тут ваш код
+/*public class TaskC2 {
+    public static void main(String[] args) {
+        int [] getNumbers = new int[20];
+        Scanner n1 = new Scanner(System.in);
+        for (int i = 0; i < getNumbers.length; i++){
+            getNumbers [i] = n1.nextInt();
+        }
+        sort(getNumbers);
     }
-
+    public static void sort(int[] array) {
+        ArrayList<Integer> newSort = new ArrayList<>();
+        for (int i = 0; i < array.length; i++){
+            newSort.add(array[i]);
+        }
+        Collections.sort(newSort);
+        Collections.reverse(newSort);
+        System.out.println(newSort);
+    }
+}*/
+public class TaskC2 {
+    public static void main(String[] args) {
+        int [] getNumbers = new int[20];
+        Scanner n1 = new Scanner(System.in);
+        for (int i = 0; i < getNumbers.length; i++){
+            getNumbers [i] = n1.nextInt();
+        }
+        sort(getNumbers);
+    }
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+        for (int i = array.length - 1; i >= 0; i--)
+            System.out.print(array[i] + "\n");
+    }
 }
